71f690c630d00e862edcf369f241524650dd2459
blackmax <blackmax1886@gmail.com>
2023-03-27 00:18:18 +0900

    chore: add eslint-disable to graphql types automatically

--- codegen.ts
+++ codegen.ts
@@ -5,7 +5,11 @@ const config: CodegenConfig = {
   schema: 'pages/api/graphql/schema/root.graphql',
   generates: {
     'pages/api/graphql/types/graphql.ts': {
-      plugins: ['typescript', 'typescript-resolvers'],
+      plugins: [
+        'typescript',
+        'typescript-resolvers',
+        { add: { content: '/* eslint-disable */' } },
+      ],
       config: {
         mappers: {
           PrismaUser: '@prisma/client/index.d#User',
