d5c02067f4f72014eaa8b830d7ae89d81b2a57c5
blackmax <blackmax1886@gmail.com>
2023-03-26 18:53:11 +0900

    Feat: apollo server

---
+++ pages/api/graphql/index.ts
@@ -0,0 +1,16 @@
+import { ApolloServer } from '@apollo/server'
+import { startServerAndCreateNextHandler } from '@as-integrations/next'
+import { loadSchemaSync } from '@graphql-tools/load'
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader'
+import { resolvers } from './resolver'
+
+const typeDefs = loadSchemaSync('pages/api/graphql/schema/root.graphql', {
+  loaders: [new GraphQLFileLoader()],
+})
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+export default startServerAndCreateNextHandler(server)
