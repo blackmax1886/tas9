27902bf36d3975cd8ac869c55b106261c2a589ae
blackmax <blackmax1886@gmail.com>
2023-03-28 11:34:08 +0900

    Refactor: fix path join

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,10 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 
-const apiBaseURI = process.env.VERCEL_URL || 'http://localhost:3000'
+const apiBaseURI = process.env.VERCEL_URL || 'http://localhost:3000/'
 
 const client = new ApolloClient({
-  uri: apiBaseURI + '/api/graphql',
+  uri: apiBaseURI + 'api/graphql',
   cache: new InMemoryCache(),
 })
 
