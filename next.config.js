f7bd941f6a63cd920ff5ed769493bd9b6f987b0f
blackmax <blackmax1886@gmail.com>
2023-03-29 00:59:14 +0900

    Fix: use async load of schema

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ['@graphql-tools/load'],
 }
 
 module.exports = nextConfig
--- pages/api/graphql/index.ts
+++ pages/api/graphql/index.ts
@@ -1,18 +1,23 @@
 import { ApolloServer } from '@apollo/server'
 import { startServerAndCreateNextHandler } from '@as-integrations/next'
-import { loadSchemaSync } from '@graphql-tools/load'
+import { loadSchema } from '@graphql-tools/load'
 import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader'
 import { resolvers } from './resolver'
 
-const typeDefs = loadSchemaSync('pages/api/graphql/schema/root.graphql', {
-  loaders: [new GraphQLFileLoader()],
-})
+const main = async () => {
+  const typeDefs = await loadSchema('pages/api/graphql/schema/root.graphql', {
+    loaders: [new GraphQLFileLoader()],
+  })
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-})
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  })
+  //TODO: GraphQLResolveInfo does not match BaseContext
+  // @ts-expect-error to be fixed
+  return startServerAndCreateNextHandler(server)
+}
 
-//TODO: GraphQLResolveInfo does not match BaseContext
-// @ts-expect-error to be fixed
-export default startServerAndCreateNextHandler(server)
+const handler = main()
+
+export default handler
