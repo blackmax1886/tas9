66b6fd1bb49d1cb3fc49d889d1ac784830b602d6
blackmax <blackmax1886@gmail.com>
2023-03-29 12:25:02 +0900

    Refactor: delete unused import

--- pages/api/graphql/index.ts
+++ pages/api/graphql/index.ts
@@ -1,7 +1,5 @@
 import { ApolloServer } from '@apollo/server'
 import { startServerAndCreateNextHandler } from '@as-integrations/next'
-// import { loadSchemaSync } from '@graphql-tools/load'
-// import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader'
 import { resolvers } from './resolver'
 
 const typeDefs = `#graphql
