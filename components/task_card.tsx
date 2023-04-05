b4277eb7d37b86c307acbfca6e5c81215eeeebc3
blackmax <blackmax1886@gmail.com>
2023-04-05 18:37:20 +0900

    Refactor: props & type definition for TaskCard

--- components/task_card.tsx
+++ components/task_card.tsx
@@ -34,31 +34,27 @@ const deleteButton = css`
   border: none;
 `
 
-const TaskCard = ({
-  task,
-  refetch,
-  openTaskDetail,
-  isSelected,
-  setSelectedTaskId,
-}: {
+type taskCardProps = {
   task: Partial<Task> | undefined
   refetch: QueryResult<GetTasksQuery>['refetch']
   openTaskDetail: (taskId: string | undefined) => void
   isSelected: boolean
   setSelectedTaskId: (taskId: string) => void
-}) => {
-  const [isDone, setIsDone] = useState(task?.done)
+}
+
+const TaskCard = (props: taskCardProps) => {
+  const [isDone, setIsDone] = useState(props.task?.done)
   const [updateTaskIsDone] = useMutation<UpdateTaskIsDoneMutation>(
     UpdateTaskIsDoneDocument,
     {
       onCompleted() {
-        refetch()
+        props.refetch()
       },
     }
   )
   const [deleteTask] = useMutation<DeleteTaskMutation>(DeleteTaskDocument, {
     onCompleted() {
-      refetch()
+      props.refetch()
     },
   })
 
@@ -66,20 +62,20 @@ const TaskCard = ({
     setIsDone(!isDone)
     updateTaskIsDone({
       variables: {
-        taskId: task?.id,
+        taskId: props.task?.id,
         isDone: !isDone,
       },
     })
   }
 
   const handleClickTask = () => {
-    openTaskDetail(task?.id)
+    props.openTaskDetail(props.task?.id)
   }
 
   const handleDeleteTask = () => {
-    deleteTask({ variables: { taskId: task?.id } })
-    if (isSelected) {
-      setSelectedTaskId('')
+    deleteTask({ variables: { taskId: props.task?.id } })
+    if (props.isSelected) {
+      props.setSelectedTaskId('')
     }
   }
 
@@ -88,7 +84,7 @@ const TaskCard = ({
     font-size: 1.5rem;
     align-items: center;
     border: 2px solid white;
-    ${isSelected &&
+    ${props.isSelected &&
     css`
       border: 2px solid #66bb6a;
     `}
@@ -128,13 +124,13 @@ const TaskCard = ({
 
   return (
     <>
-      <div key={task?.id} css={taskCard}>
+      <div key={props.task?.id} css={taskCard}>
         <div css={checkboxWrapper}>
           <input type="checkbox" css={checkbox}></input>
           <label css={checkboxWrapperLabel} onClick={handleTaskIsDone}></label>
         </div>
         <label css={taskLabel} onClick={handleClickTask}>
-          {task?.title}
+          {props.task?.title}
         </label>
         <button css={deleteButton} onClick={handleDeleteTask}>
           <Image
