6e645c1094231446409c648367d044ec5767c75c
blackmax <blackmax1886@gmail.com>
2023-04-07 12:02:38 +0900

    Feat: timetable view with calendar

---
+++ components/calendar.tsx
@@ -0,0 +1,117 @@
+import {
+  Calendar as ReactBigCalendar,
+  dayjsLocalizer,
+  stringOrDate,
+} from 'react-big-calendar'
+import dayjs from 'dayjs'
+import 'dayjs/locale/ja'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop'
+import { css } from '@emotion/react'
+import { Task } from '@/graphql/types/client'
+import styled from '@emotion/styled'
+
+dayjs.locale('ja')
+const formatString = 'YYYY-MM-DD HH:mm'
+const localizer = dayjsLocalizer(dayjs)
+const DragAndDropCalendar = withDragAndDrop(ReactBigCalendar)
+
+const tasksAsEvents = (tasks: Partial<Task>[] | undefined) => {
+  if (!tasks) {
+    return []
+  }
+  const events = tasks.map((task) => {
+    return {
+      taskId: task.id,
+      title: task.title,
+      // TODO: when start&end is null, thats ok?
+      start: dayjs(task.start, formatString).toDate(),
+      end: dayjs(task.end, formatString).toDate(),
+    }
+  })
+  return events
+}
+
+const calendar = css`
+  height: 800px;
+`
+
+const timeslot = css`
+  height: 50px;
+`
+
+export const CalendarStyleWrapper = styled.div`
+  .rbc-today {
+    background-color: #8da0b6;
+  }
+  .rbc-btn-group > button {
+    color: inherit;
+  }
+  button.rbc-active {
+    background-color: #8da0b6;
+  }
+  .rbc-toolbar button:hover {
+    background-color: #bbe2f1;
+  }
+  .rbc-toolbar button:focus {
+    color: inherit;
+    background-color: #8da0b6;
+  }
+`
+
+type TimeSlotWrapperProps = {
+  children: React.ReactNode
+}
+
+// TODO: Fix type error from TimeSlowWrapper arg
+const TimeSlotWrapper: React.FC<TimeSlotWrapperProps> = ({ children }) => {
+  return <div css={timeslot}>{children}</div>
+}
+
+const Calendar = ({
+  tasks,
+  onDropFromOutside,
+  onEventDrop,
+}: {
+  tasks: Partial<Task>[] | undefined
+  onDropFromOutside: ({
+    start,
+    end,
+  }: {
+    start: stringOrDate
+    end: stringOrDate
+  }) => void
+  onEventDrop: ({
+    event,
+    start,
+    end,
+  }: {
+    event: object
+    start: stringOrDate
+    end: stringOrDate
+  }) => void
+}) => {
+  const events = tasksAsEvents(tasks)
+  const defaultDate = dayjs().toDate()
+  const scrollToTime = dayjs().toDate()
+  return (
+    <CalendarStyleWrapper>
+      <DragAndDropCalendar
+        localizer={localizer}
+        events={events}
+        defaultView="day"
+        defaultDate={defaultDate}
+        scrollToTime={scrollToTime}
+        onDropFromOutside={onDropFromOutside}
+        onEventDrop={onEventDrop}
+        components={{
+          // @ts-expect-error to be fixed
+          timeSlotWrapper: TimeSlotWrapper,
+        }}
+        css={calendar}
+      />
+    </CalendarStyleWrapper>
+  )
+}
+
+export default Calendar
