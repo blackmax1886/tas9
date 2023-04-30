8b665a5b4dd8e08218658ea8f3a61cb8d84394d8
blackmax <blackmax1886@gmail.com>
2023-04-30 17:10:39 +0900

    Feat: draft for accout menu

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,6 @@
 import { css } from '@emotion/react'
 import { signOut } from 'next-auth/react'
+import { useState } from 'react'
 
 const header = css`
   display: flex;
@@ -17,19 +18,60 @@ const nav = css`
 `
 
 const navItem = css`
-  font-size: 1rem;
+  flex: 1 0 auto;
+  font-size: inherit;
   padding: 1rem;
 `
 
+const accountMenu = css`
+  ${navItem}
+  position: relative;
+`
+
+const accountToggleMenu = css`
+  display: flex;
+  flex-direction: column;
+  width: max-content;
+  padding: 1rem;
+  position: absolute;
+  top: 4rem;
+  right: 2rem;
+  background-color: #424242;
+  font-size: inherit;
+`
+
+const accountToggleMenuItem = css`
+  margin: 1rem 0;
+`
+
 const Header = () => {
+  const [isAccountMenuOpen, setIsAccountMenuOpen] = useState(false)
+
+  const handleAccountDeletion = () => {
+    // アカウント削除処理
+    console.log('open confirmation of account deletion')
+  }
+
   return (
     <div css={header}>
       <div css={logo}>Tas9</div>
       <nav css={nav}>
-        <button onClick={() => signOut()} css={navItem}>
+        <div onClick={() => signOut()} css={navItem}>
           Sign Out
-        </button>
-        <div css={navItem}>Account</div>
+        </div>
+        <div css={accountMenu}>
+          <div onClick={() => setIsAccountMenuOpen(!isAccountMenuOpen)}>
+            Account
+          </div>
+          {isAccountMenuOpen && (
+            <div css={accountToggleMenu}>
+              <div css={accountToggleMenuItem}>ユーザー名</div>
+              <div onClick={handleAccountDeletion} css={accountToggleMenuItem}>
+                delete account
+              </div>
+            </div>
+          )}
+        </div>
       </nav>
     </div>
   )
