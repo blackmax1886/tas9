9cd49d003cc52b4902d02260c6c9633c8206f603
blackmax <blackmax1886@gmail.com>
2023-05-23 22:34:52 +0900

    Refactor: optimistic UI for updateTaskStartEnd

--- pages/timetable.tsx
+++ pages/timetable.tsx
@@ -6,7 +6,7 @@ import { useState } from 'react'
 import { stringOrDate } from 'react-big-calendar'
 
 import Board from '@/components/Board'
-import Calendar from '@/components/Calendar'
+import Calendar, { taskAsEvent } from '@/components/Calendar'
 import Header from '@/components/Header'
 import Tabs from '@/components/Tabs'
 import QuickAdd from '@/components/task/QuickAdd'
@@ -17,7 +17,6 @@ import {
   GetTasksDocument,
   UpdateTaskStartEndMutation,
   UpdateTaskStartEndDocument,
-  Task,
   TaskSummaryFragment,
 } from '@/graphql/types/client'
 import { dayjs } from '@/lib/day'
@@ -46,73 +45,75 @@ const TimeTable = () => {
   )
   const [newTaskTitle, setNewTaskTitle] = useState('')
   const [activeTaskTab, setActiveTaskTab] = useState('tasks')
-  const [draggedTask, setDraggedTask] = useState<Partial<Task> | null>()
+  const [draggedTask, setDraggedTask] = useState<TaskSummaryFragment | null>()
   const [updateTaskStartEnd] = useMutation<UpdateTaskStartEndMutation>(
-    UpdateTaskStartEndDocument,
-    {
-      onCompleted() {
-        // Idea: to optimize, do not use refetch & use only mutation & add event to calendar manually
-        refetch()
-      },
-    }
+    UpdateTaskStartEndDocument
   )
   let tasks: TaskSummaryFragment[] = []
   if (!loading && !error && data) {
     tasks = filterByActiveTab(activeTaskTab, data.tasks)?.reverse()
   }
 
-  const handleDropFromOutside = ({
-    start,
-    end,
-  }: {
-    start: stringOrDate
-    end: stringOrDate
-  }) => {
+  const updateTaskTime = (
+    taskId: string,
+    startInput: stringOrDate,
+    endInput: stringOrDate
+  ) => {
+    const startUnixMillis = dayjs(startInput).valueOf()
+    const endUnixMillis = dayjs(endInput).valueOf()
     updateTaskStartEnd({
       variables: {
-        taskId: draggedTask?.id,
-        start: dayjs(start).valueOf(),
-        end: dayjs(end).valueOf(),
+        taskId: taskId,
+        start: startUnixMillis,
+        end: endUnixMillis,
       },
-    })
-    setDraggedTask(null)
-  }
-  const handleEventDrop = ({
-    event,
-    start,
-    end,
-  }: {
-    event: object
-    start: stringOrDate
-    end: stringOrDate
-  }) => {
-    updateTaskStartEnd({
-      variables: {
-        // @ts-expect-error to be fixed
-        taskId: event.taskId,
-        start: dayjs(start).valueOf(),
-        end: dayjs(end).valueOf(),
+      optimisticResponse: {
+        updateTask: {
+          __typename: 'Task',
+          id: taskId,
+          start: startUnixMillis,
+          end: endUnixMillis,
+        },
       },
     })
   }
 
-  const resizeEvent = ({
-    event,
-    start,
-    end,
+  const handleDropFromOutside = ({
+    start: startInput,
+    end: endInput,
   }: {
-    event: object
     start: stringOrDate
     end: stringOrDate
   }) => {
-    updateTaskStartEnd({
-      variables: {
-        // @ts-expect-error to be fixed
-        taskId: event.taskId,
-        start: dayjs(start).valueOf(),
-        end: dayjs(end).valueOf(),
-      },
-    })
+    if (!draggedTask) {
+      return
+    }
+    updateTaskTime(draggedTask.id, startInput, endInput)
+    setDraggedTask(null)
+  }
+
+  const handleEventDrop = ({
+    event,
+    start: startInput,
+    end: endInput,
+  }: {
+    event: taskAsEvent
+    start: stringOrDate
+    end: stringOrDate
+  }) => {
+    updateTaskTime(event.taskId, startInput, endInput)
+  }
+
+  const resizeEvent = ({
+    event,
+    start: startInput,
+    end: endInput,
+  }: {
+    event: taskAsEvent
+    start: stringOrDate
+    end: stringOrDate
+  }) => {
+    updateTaskTime(event.taskId, startInput, endInput)
   }
 
   return (
@@ -146,7 +147,7 @@ const TimeTable = () => {
           </Board>
           <div css={calendarWrapper}>
             <Calendar
-              tasks={data?.tasks}
+              tasks={tasks}
               onDropFromOutside={handleDropFromOutside}
               onEventDrop={handleEventDrop}
               onEventResize={resizeEvent}
