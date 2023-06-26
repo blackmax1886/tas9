03c07a7fcf66575d27195c2b4a5a7e271abcf6cb
blackmax <blackmax1886@gmail.com>
2023-06-26 19:11:07 +0900

    Refactor: apply code formatting

--- pages/api/graphql/resolver/permissions.ts
+++ pages/api/graphql/resolver/permissions.ts
@@ -1,24 +1,22 @@
-import { rule, shield } from "graphql-shield"
+import { rule, shield } from 'graphql-shield'
 
-// Define the rule
-const isCurrentUser = rule()(async (parent, { id }, context) => id === context.session.user.id)
+const isCurrentUser = rule()(
+  async (parent, { id }, context) => id === context.session.user.id
+)
 const isOwner = rule()(async (parent, args, context) => {
-    // Get the user ID from the session
-    const userId = context.session.user.id
-  
-    // Check if the user is the owner of the object by comparing the userId in the session with the userId in the args
-    return args.userId === userId
-  })
+  const userId = context.session.user.id
 
-// Apply the rule to the fields
+  return args.userId === userId
+})
+
 const permissions = shield({
-    Query: {
-      user: isCurrentUser,
-      tasks: isOwner,
-    },
-    Mutation: {
-        deleteUser: isCurrentUser,
-    }
-  })
+  Query: {
+    user: isCurrentUser,
+    tasks: isOwner,
+  },
+  Mutation: {
+    deleteUser: isCurrentUser,
+  },
+})
 
 export default permissions
