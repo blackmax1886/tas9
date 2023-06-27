8e1a1eaf8321d9a835a8c845268e1bb1f66f0b62
blackmax <blackmax1886@gmail.com>
2023-06-27 10:14:38 +0900

    Feat: Authorization for task query & mutation

--- pages/api/graphql/resolver/permissions.ts
+++ pages/api/graphql/resolver/permissions.ts
@@ -8,13 +8,23 @@ const isOwner = rule()(async (parent, args, context) => {
 
   return args.userId === userId
 })
+// name this rule
+const isTaskCreator = rule()(async (parent, args, context) => {
+  const userId = context.session.user.id
+
+  return args.input.userId === userId
+})
 
 const permissions = shield({
   Query: {
     user: isCurrentUser,
     tasks: isOwner,
+    task: isOwner,
   },
   Mutation: {
+    createTask: isTaskCreator,
+    updateTask: isOwner,
+    deleteTask: isOwner,
     deleteUser: isCurrentUser,
   },
 })
