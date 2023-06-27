b5e981d7949b11cb6a287ec1d5bf07eadba882ad
blackmax <blackmax1886@gmail.com>
2023-06-27 19:28:14 +0900

    Test: fix authorization with error msg & fetch task

--- pages/api/graphql/resolver/permissions.ts
+++ pages/api/graphql/resolver/permissions.ts
@@ -1,30 +1,54 @@
 import { rule, shield } from 'graphql-shield'
 
+import { prisma } from '@/prisma/client'
+
+interface Context {
+  session: {
+    user: {
+      id: string
+    }
+  }
+}
+
+const getUserId = (context: Context) => context.session.user.id
+
 const isCurrentUser = rule()(
-  async (parent, { id }, context) => id === context.session.user.id
+  async (parent, { id }, context: Context) =>
+    id === getUserId(context) || 'You are not the current user.'
 )
-const isOwner = rule()(async (parent, args, context) => {
-  const userId = context.session.user.id
 
-  return args.userId === userId
+const isOwner = rule()(async (parent, args, context: Context) => {
+  return args.userId === getUserId(context) || 'You are not the owner.'
 })
-// name this rule
-const isTaskCreator = rule()(async (parent, args, context) => {
-  const userId = context.session.user.id
 
-  return args.input.userId === userId
+const isTaskCreator = rule()(async (parent, args, context: Context) => {
+  return (
+    args.input.userId === getUserId(context) ||
+    'You are not the owner of task you create.'
+  )
+})
+
+const isTaskOwner = rule()(async (parent, args, context: Context) => {
+  const taskId = args.taskId || args.id
+  const task = await prisma.task.findUnique({
+    where: { id: taskId },
+  })
+  return (
+    (!!task && task.userId === getUserId(context)) ||
+    'You are not the owner of this task.'
+  )
 })
 
 const permissions = shield({
   Query: {
     user: isCurrentUser,
     tasks: isOwner,
-    task: isOwner,
+    task: isTaskOwner,
   },
   Mutation: {
     createTask: isTaskCreator,
-    updateTask: isOwner,
-    deleteTask: isOwner,
+    updateTask: isTaskOwner,
+    deleteTask: isTaskOwner,
     deleteUser: isCurrentUser,
   },
 })
